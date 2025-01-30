ed28b057734d8635f5c449950b224f74d8c6498f
meirza23 <mirzaetka23@gmail.com>
2025-01-30 04:46:08 +0300

    .

--- main.go
+++ main.go
@@ -118,68 +118,6 @@ func formatTime(seconds int) string {
 	return fmt.Sprintf("%02d:%02d", mins, secs)
 }
 
-func ShowSongs() {
-
-	files, err := os.ReadDir(".")
-	if err != nil {
-		fmt.Println("Dizin okunamadı: ", err)
-		return
-	}
-
-	songFiles := make([]string, 0)
-	for _, file := range files {
-		if !file.IsDir() && strings.HasSuffix(file.Name(), ".mp3") {
-			songFiles = append(songFiles, file.Name())
-		}
-	}
-
-	if len(songFiles) == 0 {
-		fmt.Println("Kütüphanede şarkı bulunamadı!")
-		time.Sleep(2 * time.Second)
-		return
-	}
-
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		clearScreen()
-		fmt.Println("Tüm Şarkılar:")
-		for i, file := range songFiles {
-			fmt.Printf("\n%d. %s", i+1, file)
-		}
-
-		fmt.Println("\nÇalmak için numara girin (Ana menü için 0): ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-
-		if input == "0" {
-			return
-		}
-
-		num, err := strconv.Atoi(input)
-		if err != nil || num < 1 || num > len(songFiles) {
-			fmt.Printf("Geçersiz seçim! (1-%d arası değer girin)\n", len(songFiles))
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		selectedSong := songFiles[num-1]
-		playLocalSong(selectedSong)
-	}
-}
-
-func ShowPlaylists() {
-	originalDir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Dizin alınamadı:", err)
-		return
-	}
-	err = os.Chdir(originalDir)
-	if err != nil {
-		fmt.Println("Dizin değiştirilemedi:", err)
-	}
-}
-
 func main() {
 	directories := []string{"./Playlists", "./Songs", "./Playlists/Favourites"}
 	for _, dir := range directories {
